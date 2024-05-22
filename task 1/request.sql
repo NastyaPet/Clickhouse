292bbfc9bec0b6e73219172f9cfe0509c9fb3782
NastyaPet <47759744+NastyaPet@users.noreply.github.com>
2024-05-22 11:58:12 +0300

    Update request.sql

--- task 1/request.sql
+++ task 1/request.sql
@@ -1,1 +1,22 @@
+CREATE DATABASE Anastasia;
+
+CREATE TABLE Anastasia.test (
+  a UInt32,
+  b String,
+  c Enum8('a' = 1, 'b' = 2)
+) Engine = Log();
+
+SELECT * FROM Anastasia.test
+WHERE b like '%abc%' AND a > 10;
+
+
+INSERT INTO Anastasia.test (a,b,c)
+SELECT 
+  rand() as a,
+  fuzzBits(materialize('abcdefabc'),0.1) as b,
+  ['a', 'b'][toInt32(randUniform(1,3))]
+FROM numbers(100000);
+
+SELECT * FROM Anastasia.test WHERE b like '%abc%' AND a > 10 LIMIT 3
+
 
